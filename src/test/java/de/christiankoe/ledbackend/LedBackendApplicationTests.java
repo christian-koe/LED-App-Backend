05bde48c7f65cfe9ccd0690a3120efd1d466f205
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2022-11-13 12:30:25 +0100

    Initial Commit

---
+++ src/test/java/de/christiankoe/ledbackend/LedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.christiankoe.ledbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
